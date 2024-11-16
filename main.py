f46a5e7ec1d23b927ddda48be0180c8508c9565d
Kemran <morpeh2005ka@gmail.com>
2024-11-17 00:32:45 +0300

    It s over IAD has fallen

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 """Imports"""
+from collections import defaultdict
+import random
+from nltk.tokenize import word_tokenize
+
+
+def ngram(text: str, n: int = 3) -> str:
+    """
+    Function for calculation ngram
+    """
+    words = word_tokenize(text)
+    ngrams = zip(*[words[i:] for i in range(n)])
+    trans = defaultdict(list)
+    init = []
+    for i in ngrams:
+        if i[0] == '.':
+            init.append(i[1:n-1])
+        trans[i[0:n-1]].append(i[-1])
+    result = random.choice(init)
+    fr = ['.']
+    res_true = [result[0]]
+    while True:
+        candidates = trans[tuple(fr + list(result))]
+        next_word = random.choice(candidates)
+        fr = [result[0]]
+        result = list(result[1:]) + [next_word]
+        res_true.append(result[0])
+        if result[0] == '.':
+            return " ".join(res_true)
 
 
 def main() -> None:
     """
     main function
     """
-    print()
+    with open("data.txt", "r+", encoding="UTF8") as f:
+        text = f.read()
+        print(f"Trigram {ngram(text, 3)}")
+        print(f"Fourgram {ngram(text, 4)}")
 
 
 if __name__ == "__main__":
